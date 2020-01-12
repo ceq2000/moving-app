07c1668fa201c9744968fc6ad7605784e6953451
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-01-12 15:02:13 -0500

    Edited line 10 of seedDB to fix minor problem

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -7,8 +7,8 @@ const db = require("../models");
 // This file empties the Book and User collections and inserts the seeds below
 // mongoose.connect(
 //   process.env.MONGODB_URI ||
-//   "mongodb://localhost/moving-app"
-);
+//   "mongodb://localhost/moving-app");
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
