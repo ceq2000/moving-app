e58ce646c16c32e71ae4e38f9ae2d59ccd9431a9
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-01-16 20:24:31 -0500

    changed client/public to client/build in server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(passport.initialize());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
