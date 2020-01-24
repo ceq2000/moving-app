2752f22c246f7e0b779215f224bd2d9fed99b905
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-01-23 21:46:15 -0500

    added navlinks in navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,48 +1,42 @@
 import React from "react";
 import { connect } from 'react-redux'
-import { Button, Navbar } from 'react-bootstrap';
+import { Button, Navbar, NavItem } from 'react-bootstrap';
+import { NavLink } from "reactstrap";
 import { Link } from "react-router-dom";
+import './App.scss';
 
 import { onLogout } from '../../redux/actions'
-import { PromiseProvider } from "mongoose";
-import './App.scss';
+
 const Nav = (props) => {
-  
-  return <Navbar className='navbar-expand-lg justify-content-center'>
-    <Navbar.Brand className='type' href="/"> Tali-Home</Navbar.Brand>
-   
-    {
-      props.user ?
-        <Button onClick={props.onLogout}>Logout</Button> :
-        <Link to={"/login"}>
-          {/* <Button className='btn btn-light-danger'>Login</Button> */}
-        </Link>
-    }
-  </Navbar>;
+  return   <Navbar className='navbar-expand-lg justify-content-between'>
+  <Navbar.Brand className="type" href="/">Talli-Home</Navbar.Brand>
+  <Navbar className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={Link} to={'/page-1'}>Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to={'/page-2'}>Category</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to={'/page-3'}>About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to={'/page-4'}>Contact</NavLink>
+          </NavItem>
+       </Navbar>
+  {
+    props.user ?
+      <Button onClick={props.onLogout}>Logout</Button> :
+      <Link to={"/login"}>
+        <Button>Login</Button>
+      </Link>
+  }
+</Navbar>;
 };
 
 export default connect(
-  // mapStateToProps,
-  state => ({user: state.user.details }),
-  // mapDispatchToProps,
+  // mapStateToProps
+  state => ({user: state.user.details}),
+  // mapDispatchToProps
   { onLogout }
 )(Nav);
-// const Nav = (props) => {
-//   return   <Navbar className='navbar-expand-lg justify-content-right'>
-//   <Navbar.Brand className='type' href="/">Home Inventory</Navbar.Brand>
-//   {
-//     props.user ?
-//       <Button onClick={props.onLogout}>Logout</Button> :
-//       <Link to={"/login"}>
-//         <Button className='btn btn-dark-success'>Login</Button>
-//       </Link>
-//   }
-// </Navbar>;
-// };
-
-// export default connect(
-//   // mapStateToProps
-//   state => ({user: state.user.details}),
-//   // mapDispatchToProps
-//   { onLogout }
-// )(Nav);
