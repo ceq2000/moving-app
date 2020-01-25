55c6cf4d855dd9c04c618651997a74fb5a1158c3
Oscar Enriquez <macair@Oscars-MacBook-Air.local>
2020-01-25 12:36:26 -0500

    david push

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -17,7 +17,9 @@ export function Input(props) {
 export function TextArea(props) {
   return (
     <div className="form-group">
-      <textarea className="form-control" rows="8" {...props} />
+      <textarea className="form-control" rows="5" {...props} />
+      
+
     </div>
   );
 }
