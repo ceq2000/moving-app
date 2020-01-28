b363d9dcec78f268da1f8fe37504cafff8f2785c
David Levens <52017470+Davidlevens@users.noreply.github.com>
2020-01-28 18:48:05 -0500

    Minor change to server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(passport.initialize());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/index.html"));
+  app.use(express.static("client/index.html"));
 }
 // Add routes, both API and view
 app.use(routes);
